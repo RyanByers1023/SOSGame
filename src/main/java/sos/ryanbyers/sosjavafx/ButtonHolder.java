d4520a0f304b06c0475bbda1819af381a22109c8
Ryan Byers <ryanbyers1023@gmail.com>
2024-10-13 18:17:52 -0500

    Add: objects to hold, control, and provide easy access to attributes related to the GUI and the board.

---
+++ src/main/java/sos/ryanbyers/sosjavafx/ButtonHolder.java
@@ -0,0 +1,49 @@
+package sos.ryanbyers.sosjavafx;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+public class ButtonHolder {
+    public RadioButton simpleGamemode;
+    public RadioButton generalGamemode;
+
+    public RadioButton blueS;
+    public RadioButton blueO;
+
+    public RadioButton redS;
+    public RadioButton redO;
+
+    public Button startButton;
+
+    public ButtonHolder(){
+        //gamemode radio buttons
+        simpleGamemode = new RadioButton("Simple game");
+        generalGamemode = new RadioButton("General game");
+
+        //set correct group (gamemode group)
+        ToggleGroup gamemodeToggle = new ToggleGroup();
+        simpleGamemode.setToggleGroup(gamemodeToggle);
+        generalGamemode.setToggleGroup(gamemodeToggle);
+
+        //Blue player piece choice radio buttons
+        blueS = new RadioButton("S");
+        blueO = new RadioButton("O");
+
+        //set correct group (bluePiece group)
+        ToggleGroup bluePieceToggle = new ToggleGroup();
+        blueS.setToggleGroup(bluePieceToggle);
+        blueO.setToggleGroup(bluePieceToggle);
+
+        //Red player piece choice radio buttons
+        redS = new RadioButton("S");
+        redO = new RadioButton("O");
+
+        //set correct group (redPiece group)
+        ToggleGroup redPieceToggle = new ToggleGroup();
+        redS.setToggleGroup(redPieceToggle);
+        redO.setToggleGroup(redPieceToggle);
+
+        startButton = new Button("Start Game");
+    }
+}
