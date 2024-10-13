d0b2c71e2cd8a06c676b93958892b716da02ee38
Ryan Byers <ryanbyers1023@gmail.com>
2024-10-13 18:17:48 -0500

    Add: objects to hold, control, and provide easy access to attributes related to the GUI and the board.

---
+++ src/main/java/sos/ryanbyers/sosjavafx/HBoxHolder.java
@@ -0,0 +1,31 @@
+package sos.ryanbyers.sosjavafx;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+
+public class HBoxHolder {
+    public HBox gameOptionsBox;
+    public HBox gameSpaceBox;
+    public HBox playerTurnBox;
+    public HBox startBox;
+
+    public HBoxHolder() {
+        //hbox for game mode selection radio buttons
+        gameOptionsBox = new HBox(15);
+        gameOptionsBox.setPadding(new Insets(15));
+        gameOptionsBox.setAlignment(Pos.CENTER);
+
+        //holds the grid box and red and blue player piece choices boxes
+        gameSpaceBox = new HBox(10);
+        gameSpaceBox.setAlignment(Pos.CENTER);
+
+        //holds text that tells the user which player's turn it is
+        playerTurnBox = new HBox(10);
+        playerTurnBox.setAlignment(Pos.CENTER);
+
+        //holds the button to start the game
+        startBox = new HBox(10);
+        startBox.setAlignment(Pos.CENTER);
+    }
+}
